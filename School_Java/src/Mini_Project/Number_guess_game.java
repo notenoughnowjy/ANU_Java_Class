150f8aa036021cfb3f3fd98cb9f628ee0c823d06
ParkJunYeong <jyp4188@gmail.com>
2024-03-26 15:14:43 +0900

    [2024.03.26] mini project: 숫자 추측 게임

--- School_Java/src/Mini_Project/Number_guess_game.java
+++ School_Java/src/Mini_Project/Number_guess_game.java
@@ -5,35 +5,29 @@ import java.util.Scanner;
 public class Number_guess_game {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        랜덤 숫자 생성
-        double double_random_number = Math.random() * 100;
-//        랜덤 숫자를 int형으로 변환
-        int Int_random_number = (int) double_random_number;
-
+        int answer;
         int try_count = 0;
-        while(true){
-            System.out.print("정답을 추축해보시오: ");
-//            추측 값 입력받기
-            int answer = scanner.nextInt();
 
-//            추측값이 정답보다 크다면 HIGH
-            if(Int_random_number < answer){
+        // 임의의 정수 생성 (예: 1에서 100 사이의 임의의 정수)
+        int Int_random_number = (int) (Math.random() * 100) + 1;
+
+        do {
+            System.out.print("정답을 추축해보시오: ");
+            // 추측 값 입력받기
+            answer = scanner.nextInt();
+
+            // 추측값이 정답보다 크다면 HIGH
+            if (Int_random_number < answer) {
                 System.out.println("HIGH");
                 try_count++;
             }
-
-//            추측값이 정답보다 작다면 LOW
-            if(Int_random_number > answer){
+            // 추측값이 정답보다 작다면 LOW
+            else if (Int_random_number > answer) {
                 System.out.println("LOW");
                 try_count++;
             }
-
-//            추측값과 정답이 동일하다면 축하 메시지와 함께 시도횟수 출력후 종료
-            if(answer == Int_random_number){
-                System.out.println("축하합니다. " + "시도횟수=" + ++try_count);
-                break;
-            }
-        }
-
+        } while (Int_random_number != answer);
+        System.out.println("축하합니다. " + "시도횟수=" + ++try_count);
     }
 }
+
