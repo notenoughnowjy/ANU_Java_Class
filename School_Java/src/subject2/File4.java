2e036915bb0ab9a4f50fda8fed8b076777199396
ParkJunYeong <jyp4188@gmail.com>
2024-04-15 18:35:15 +0900

    [2024.04.15] 자바 Programming Exercise 1번 해결

--- School_Java/src/subject2/File4.java
+++
@@ -1,9 +0,0 @@
-package subject2;
-
-
-/*
-국립안동대학교 컴퓨터공학과 박준영
-20181113 박준영
- */
-public class File4 {
-}
---
+++ School_Java/src/subject2/StudentTest.java
@@ -0,0 +1,31 @@
+package subject2;
+
+
+/*
+국립안동대학교 컴퓨터공학과 박준영
+20181113 박준영
+ */
+
+class Student{
+    String name;
+    int StudentID;
+    int age;
+    public Student(String name, int StudentID, int age){
+        this.name = name;
+        this.StudentID = StudentID;
+        this.age = age;
+    }
+
+    public void printout(){
+        System.out.println("학생의 이름: " + name);
+        System.out.println("학생의 학번: " + StudentID);
+        System.out.println("학생의 나이: " + age);
+    }
+}
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student1 = new Student("Kim", 20180001, 20);
+        student1.printout();
+    }
+}
